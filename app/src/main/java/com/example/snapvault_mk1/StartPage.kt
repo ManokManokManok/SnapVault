86a99511ae0c6a82663d729f2319c23a1d32f6ee
ManokManokManok <kenchitomeldan@gmail.com>
2024-09-20 13:58:20 +0800

    added start screen with bg and changed logo

--- app/src/main/java/com/example/snapvault_mk1/StartPage.kt
+++ app/src/main/java/com/example/snapvault_mk1/StartPage.kt
@@ -1,6 +1,13 @@
 package com.example.snapvault_mk1
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.animation.AlphaAnimation
+import android.view.animation.TranslateAnimation
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.VideoView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +23,52 @@ class StartPage : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
-}
+
+        val startlogo = findViewById<ImageView>(R.id.startlogo)
+        val starttitle = findViewById<TextView>(R.id.starttitle)
+        val bgVid = findViewById<VideoView>(R.id.background)
+
+        startlogo.setOnClickListener{
+            val Intent = Intent (this,MainActivity::class.java)
+            startActivity(Intent)
+        }
+
+        val videoUri = Uri.parse("android.resource://${packageName}/${R.raw.startbackground}")
+
+        //HINDI PA ITO FINAL HA LALAGYAN PA NG MAGANDANG VID
+
+        bgVid.setVideoURI(videoUri)
+        bgVid.start()
+
+        bgVid.setOnCompletionListener {
+            bgVid.start()
+        }
+
+        val animation = TranslateAnimation(
+            0f,
+            0f,
+            1000f,
+            0f
+        )
+        animation.duration = 1100
+        animation.fillAfter = true
+
+        startlogo.startAnimation(animation)
+
+        val fadeInAnimation = AlphaAnimation(0f, 1f).apply {
+            duration = 500
+            fillAfter = true
+        }
+        animation.setAnimationListener(object : android.view.animation.Animation.AnimationListener {
+            override fun onAnimationStart(animation: android.view.animation.Animation?) {
+            }
+
+            override fun onAnimationEnd(animation: android.view.animation.Animation?) {
+                starttitle.startAnimation(fadeInAnimation)
+            }
+
+            override fun onAnimationRepeat(animation: android.view.animation.Animation?) {
+            }
+
+    })
+}}
